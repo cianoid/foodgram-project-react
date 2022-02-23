b1992bdee66195fc3186c345dff1f9041d2f3f92
Igor Shatava <cianoid@yandex.ru>
2022-02-23 17:50:29 +0900

    add api endpoints for Tag

---
+++ backend/foodgram/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import TagViewSet
+
+
+router = routers.DefaultRouter()
+
+router.register('tags', TagViewSet, basename='tags')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from receipts.models import Tag
+from .serializers import TagSerializer
+
+
+class ListRetrieveViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
+                          mixins.RetrieveModelMixin):
+    permission_classes = (AllowAny, )
+    lookup_field = 'id'
+
+
+class TagViewSet(ListRetrieveViewSet):
+    search_fields = ('slug',)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
