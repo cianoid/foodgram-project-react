4279badbfa31b0b4818f4bd3cf283143305b7f75
Igor Shatava <cianoid@yandex.ru>
2022-03-07 19:36:31 +0900

    add tags tests

--- backend/foodgram/api/tests/test_tags.py
+++ backend/foodgram/api/tests/test_tags.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from django.urls import include, path
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient, APITestCase, URLPatternsTestCase
+
+from recipes.models import Ingredient, IngredientRecipeRelation, Recipe, Tag
 
 User = get_user_model()
 
@@ -11,26 +14,68 @@ class APITests(APITestCase, URLPatternsTestCase):
     urlpatterns = [
         path('api/', include('api.urls')),
     ]
+    fixtures = ('users.json', 'recipes.json',)
+
+    user: User
+    tag: Tag
+
+    user_client: APIClient
+    anon_client: APIClient
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
 
+        cls.user = get_object_or_404(User, pk=2)
+        cls.tag = get_object_or_404(Tag, pk=1)
+
     @classmethod
     def tearDownClass(cls):
-        pass
+        IngredientRecipeRelation.objects.all().delete()
+        Tag.objects.all().delete()
+        Ingredient.objects.all().delete()
+        Recipe.objects.all().delete()
+        User.objects.all().delete()
 
     def setUp(self):
-        pass
+        self.anon_client = APIClient()
+
+        self.user_client = APIClient()
+        self.user_client.force_authenticate(user=self.user)
+
+    def __tag_list(self, apiclient):
+        endpoint = reverse('api:tags-list')
+
+        response = apiclient.get(endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['id'], self.tag.pk)
+        self.assertEqual(response.data[0]['slug'], self.tag.slug)
+        self.assertEqual(len(response.data), Tag.objects.all().count())
+
+    def __tag_detail(self, apiclient):
+        endpoint = reverse('api:tags-detail', kwargs={'pk': self.tag.pk})
+
+        response = apiclient.get(endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], self.tag.pk)
+        self.assertEqual(response.data['slug'], self.tag.slug)
 
     def test_anon_tag_list(self):
         """Неавторизованные пользователи. Список тегов."""
 
+        self.__tag_list(self.anon_client)
+
     def test_anon_tag_detail(self):
         """Неавторизованные пользователи. Получение тега."""
+
+        self.__tag_detail(self.anon_client)
 
     def test_user_tag_list(self):
         """Авторизованные пользователи. Список тегов."""
 
+        self.__tag_list(self.user_client)
+
     def test_user_tag_detail(self):
         """Авторизованные пользователи. Получение тега."""
+
+        self.__tag_detail(self.user_client)
